f46d3032a9cb94551f9fe3776c972287a7169270
Ananjan <daspoddarananjan@gmail.com>
2026-09-09 14:05:22 +0530

    Commit -8 ig

--- myProject20/blog/models.py
+++ myProject20/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    
+    category = models.CharField(max_length=100,blank=True,null=True)
+
     def __str__(self):
         return self.title
